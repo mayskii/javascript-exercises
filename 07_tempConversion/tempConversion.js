dd2e662f3aadb15d947acf4a7b6fe55bd18fea0d
Mayskii <mayzakharova@gmail.com>
2023-05-04 22:29:55 -0500

    test-2

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(tFahrenheit) {
 
-const convertToFahrenheit = function() {
+  let Celsium = ((Math.round(tFahrenheit * 10) / 10) - 32) * 5/9;
+  return parseFloat(Celsium.toFixed(1));
+}
+const convertToFahrenheit = function(tCelsium) {
+  let Fahrenheit = ((Math.round(tCelsium * 10) / 10)* 9/5 + 32);
+  return parseFloat(Fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
